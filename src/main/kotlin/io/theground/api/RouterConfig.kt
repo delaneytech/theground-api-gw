f9cfb8b81035b9ddffe01fd9e45c18eac3766674
rich delaney <delaney.tech.14@gmail.com>
2019-11-06 10:57:21 -0500

    Stream API incorporate changes for Registry API and Deployment API

--- src/main/kotlin/io/theground/api/RouterConfig.kt
+++ src/main/kotlin/io/theground/api/RouterConfig.kt
@@ -1,22 +1,39 @@
 package io.theground.api
 
+import org.springframework.cloud.client.discovery.DiscoveryClient
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@EnableDiscoveryClient
 class RouterConfig {
 
     @Bean
-    fun routes (builder : RouteLocatorBuilder): RouteLocator =
-            builder.routes()
-                    .route {
-                        it.path("/functions*")
-                                .uri("http://function-api.api/functions")}
-                    .route {
-                        it.path("/functions/*")
-                                .uri("http://function-api.api/functions")
-                    }
-                    .build()
+    fun discovery (discoveryClient: DiscoveryClient) =
+            DiscoveryClientRouteDefinitionLocator(discoveryClient, DiscoveryLocatorProperties())
+
+//    @Bean
+//    fun routes (builder : RouteLocatorBuilder): RouteLocator =
+//            builder.routes()
+//                    .route {
+//                        it.path("/functions*")
+//                                .uri("http://localhost:8081/functions")}
+//                    .route {
+//                        it.path("/functions/*")
+//                                .uri("http://localhost:8081/functions")
+//                    }
+//                    .route {
+//                        it.path("/topics*")
+//                                .uri("http://localhost:8084/topics")
+//                    }
+//                    .route {
+//                        it.path("/topics/*")
+//                                .uri("http://localhost:8084/topics")
+//                    }
+//                    .build()
 }
